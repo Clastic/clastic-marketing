9e42fa00c48b659bc0ab0342def278a225eb9557
Dries De Peuter <dries@nousefreak.be>
2015-03-12 23:18:58 +0100

    Add cache headers.

--- app/AppCache.php
+++ app/AppCache.php
@@ -6,4 +6,16 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+    protected function getOptions()
+    {
+        return array(
+            'debug' => false,
+            'default_ttl' => 300,
+            'private_headers' => array(),
+            'allow_reload' => false,
+            'allow_revalidate' => false,
+            'stale_while_revalidate' => 60,
+            'stale_if_error' => 86400,
+        );
+    }
 }
